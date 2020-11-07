ff8210769c72bdf03bbb86004e044d01d25b4015
7777sea <923384777@qq.com>
2020-11-07 11:58:35 +0800

    blog init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Mercury',
+    description: 'Mercury Nook',
+    head: [
+        ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }]
+    ],
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+    }
+}
