43fb1eb912656d5c0911c3469f23c153199de80b
liulu <liulu@hosecloud.com>
2020-11-09 18:09:28 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ set -e
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹  //$(date)
 cd docs/.vuepress/dist
 touch README.md
 echo '# welcome to Mercury Blog' > README.md
@@ -16,7 +15,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:7777sea/7777sea.github.io.git master
 
 cd -
