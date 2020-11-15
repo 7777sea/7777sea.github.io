bc03d46aa162ea7fda085807e65f238b7fc4b6e4
7777sea <923384777@qq.com>
2020-11-15 09:19:37 +0800

    blog build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,24 @@
 module.exports = {
     title: 'Mercury',
-    description: 'Mercury Nook',
+    description: 'Welcome to Mercury Nook',
     head: [
         ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }]
     ],
+    serviceWorker: true,
     themeConfig: {
         logo: '/assets/img/logo.png',
         repo: 'https://github.com/7777sea/7777sea.github.io',
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-        ]
+            { text: 'Blog', link: '/Blog/' },
+            { text: 'Record', link: '/Record/' },
+        ],
+        sidebar: {
+            "/articles/": [
+              "/articles/home.html",
+            ],
+          },
+          sidebarDepth: 2,
+          smoothScroll: true,
     }
 }
